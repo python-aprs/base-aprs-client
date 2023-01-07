f6a93c41a8d810693ac861bde529c4ec91077513
Masen Furer <m_github@0x26.net>
2023-01-07 01:06:56 -0800

    client: send_status and send_posititon
    helper function for send_frame
    
    allow destination to be set per-client

--- src/base_aprs_client/client.py
+++ src/base_aprs_client/client.py
@@ -5,7 +5,15 @@ import typing as t
 
 import attrs
 
-from aprs import APRSFrame, Message, Position
+from aprs import (
+    APRSFrame,
+    DataType,
+    Message,
+    Position,
+    InformationField,
+    StatusReport,
+    PositionReport,
+)
 from ax253 import Address
 
 
@@ -25,6 +33,7 @@ class MessageAckFuture(Future):
 class Client:
     mycall = attrs.field(converter=Address.from_any)
     sync_frame_io = attrs.field()
+    destination = attrs.field(default="APZ069", converter=Address.from_any)
     next_message_sequence_number = attrs.field()
     outstanding_messages = attrs.field(factory=dict, init=False)
 
@@ -43,8 +52,22 @@ class Client:
 
     def on_frame(self, frame: APRSFrame) -> None:
         print(frame)
-        if isinstance(frame.info, Message) and frame.info.addressee.decode() == str(self.mycall):
+        if isinstance(frame.info, Message) and frame.info.addressee.decode() == str(
+            self.mycall
+        ):
             self.on_message(frame.info, frame)
+
+    def send_frame(
+        self, info: InformationField, path: t.Optional[t.List[Address]] = None
+    ) -> APRSFrame:
+        frame = APRSFrame(
+            destination=self.destination,
+            source=self.mycall,
+            path=path or [],
+            info=info,
+        )
+        self.sync_frame_io.write(frame)
+        return frame
 
     def send_message(
         self,
@@ -53,20 +76,27 @@ class Client:
         ack: bool = False,
         overflow: OverflowDisposition = OverflowDisposition.Truncate,
     ) -> MessageAckFuture:
-        self.sync_frame_io.write(
-            APRSFrame(
-                destination="APZ069",
-                source=self.mycall,
-                path=[],
-                info=Message(
-                    raw=b"",
-                    data_type=None,
-                    data=b"",
-                    addressee=str(recipient).encode(), text=body.encode()))
+        frame = self.send_frame(
+            info=Message(
+                raw=b"",
+                data_type=None,
+                data=b"",
+                addressee=str(recipient).encode(),
+                text=body.encode(),
+            ),
         )
 
     def send_status(self, status: str) -> APRSFrame:
-        pass
+        return self.send_frame(
+            info=StatusReport(raw=b"", data_type=None, data=b"", status=status.encode())
+        )
 
     def send_position(self, position: Position) -> APRSFrame:
-        pass
+        return self.send_frame(
+            info=PositionReport(
+                raw=b"",
+                data_type=DataType.POSITION_W_O_TIMESTAMP_MSG,
+                data=b"",
+                position=position,
+            )
+        )
