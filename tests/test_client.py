8559c3cf50d562a5b5ba90a9f2bb18e9cfed806c
Masen Furer <m_github@0x26.net>
2023-01-07 01:07:43 -0800

    test_client: basic smoke tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,36 +1,21 @@
-import time
-
-import attrs
-import pytest
-
-from aprs import APRSFrame, Message, TCP
-from base_aprs_client import Client
-
-
-@attrs.define
-class MessageSavingClient(Client):
-    client2_rxd = attrs.field(factory=list)
-
-    def on_message(self, message: Message, frame: APRSFrame) -> None:
-        super().on_message(message, frame)
-        self.client2_rxd.append(frame)
-    
-
-@pytest.fixture
-def client2(server):
-    with TCP(
-        host="127.0.0.1",
-        port=server.port,
-    ) as sync_frame_io:
-        yield MessageSavingClient(
-            mycall="CLIENT2",
-            sync_frame_io=sync_frame_io,
-        )
+from aprs import Position
 
 
 def test_message(client1, client2):
-    print("connect now")
-    time.sleep(5)
     client1.send_message(client2.mycall, "This is the message")
     client2.read(min_frames=1)
-    assert client2.client2_rxd
+    assert client2.messages[0].text == b"This is the message"
+
+
+def test_status(client1, client2):
+    client1.send_status("foo")
+    client2.read(min_frames=1)
+    assert client2.frames[0].info.raw == b">foo"
+
+
+def test_position(client1, client2):
+    pos = Position()
+    sent_frame = client1.send_position(position=pos)
+    assert sent_frame.info._position == pos
+    client2.read(min_frames=1)
+    assert client2.frames[0].info._position == pos
