f5f672391399369c2068af417fc55699fb88413a
Masen Furer <m_github@0x26.net>
2023-01-07 01:07:21 -0800

    conftest: all clients save frames and messages during test

--- tests/conftest.py
+++ tests/conftest.py
@@ -106,13 +106,27 @@ def server():
     s.join()
 
 
+@attrs.define
+class FrameSavingClient(Client):
+    frames = attrs.field(factory=list)
+    messages = attrs.field(factory=list)
+
+    def on_frame(self, frame):
+        super().on_frame(frame)
+        self.frames.append(frame)
+
+    def on_message(self, message, frame):
+        super().on_message(message, frame)
+        self.messages.append(message)
+
+
 @pytest.fixture
 def client1(server):
     with TCP(
         host="127.0.0.1",
         port=server.port,
     ) as sync_frame_io:
-        yield Client(
+        yield FrameSavingClient(
             mycall="CLIENT1",
             sync_frame_io=sync_frame_io,
         )
@@ -124,7 +138,7 @@ def client2(server):
         host="127.0.0.1",
         port=server.port,
     ) as sync_frame_io:
-        yield Client(
+        yield FrameSavingClient(
             mycall="CLIENT2",
             sync_frame_io=sync_frame_io,
         )
